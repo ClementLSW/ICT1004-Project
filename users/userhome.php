7384d9317d0543d2c673339067eb219f2faa83dd
JustinSoh <hoggersoh@gmail.com>
2020-03-05 01:04:05 +0800

    [User Portal] Added some Design

---
+++ users/userhome.php
@@ -0,0 +1,17 @@
+
+<section class="bg">
+    <section id="direction_container" class="row justify-content-center">
+        <article id="direction_header" class="justify-content-center" my-auto>
+            <h1 class="direction_text">
+                WHERE WOULD YOU LIKE TO GO ? <br>
+                <small class="tag-line text-muted">we make it possible</small>
+            </h1>
+          
+
+        </article>
+    </section>
+</section>
+
+
+
+<section>
