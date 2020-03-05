7230228626a9d21e520835740043af533f720978
JustinSoh <hoggersoh@gmail.com>
2020-03-06 07:49:34 +0800

    Updated user input
    nothing much

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <title>This is the title of the webpage!</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
@@ -7,10 +7,18 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
+        <script src="../resources/js/lib/userhome.js"></script>
+
         <!-- External Style Sheet -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"></link>
+        <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/darkly/bootstrap.min.css" rel="stylesheet" integrity="sha384-rCA2D+D9QXuP2TomtQwd+uP50EHjpafN+wruul0sXZzX/Da7Txn4tB9aLMZV4DZm" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet" />
+        
+        <!-- Own Style Sheet -->
         <link rel="stylesheet" href="resources/css/main.css"></link>
-        <link rel="stylesheet" href="resources/css/userhome.css"></link>
+       
+
     </head>
     <body>
         <?php include 'navigation.php';?>
--- users/userhome.php
+++ users/userhome.php
@@ -1,17 +1,21 @@
-
-<section class="bg">
+<!-- Stylesheet  -->
+<link rel="stylesheet" href="resources/css/userhome.css"></link>
+<section role="main" class="bg">
     <section id="direction_container" class="row justify-content-center">
         <article id="direction_header" class="justify-content-center" my-auto>
-            <h1 class="direction_text">
-                WHERE WOULD YOU LIKE TO GO ? <br>
-                <small class="tag-line text-muted">we make it possible</small>
-            </h1>
-          
-
+            <h1 class="direction_text extra-bold">Enter your destination below <br></h1>
         </article>
     </section>
-</section>
+    <section id="user_input_container" class="row">
+        <div class="col-sm-2 empty_div"></div>
+        <form id="user_form" class="col-sm-8">
+            <div class="form-group ">
+                <select class="user_input js-example-basic-single" id="destinationInput">
+                    <option value="1">Singapore Expo</option>
+                    <option value="2">Suntec</option>
+                </select>
+            </div>
+        </form>
+        <div class="col-sm-2 empty_div"></div>
+    </section>
 
-
-
-<section>
