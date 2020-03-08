6b493628361ed2a07fae0f18fe84ee8c594e4981
shun-h2 <46297054+shun-h2@users.noreply.github.com>
2020-03-08 21:01:16 +0800

    Sample SQL
    Sample SQL to test web page functions. (temporary)

---
+++ carpark.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 08, 2020 at 01:58 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `carpark`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `area`
+--
+
+CREATE TABLE `area` (
+  `area_id` varchar(5) NOT NULL,
+  `type` varchar(45) NOT NULL,
+  `occupancy` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `connection`
+--
+
+CREATE TABLE `connection` (
+  `id` varchar(5) NOT NULL,
+  `weight` int(11) NOT NULL,
+  `cost` int(11) NOT NULL,
+  `start_node` varchar(45) NOT NULL,
+  `end_node` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `username` varchar(45) NOT NULL,
+  `fname` varchar(45) DEFAULT NULL,
+  `lname` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `contact` int(8) DEFAULT NULL,
+  `permissions` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`username`, `fname`, `lname`, `password`, `email`, `contact`, `permissions`) VALUES
+('andysmith', 'andy', 'smith', 'anddy', 'andysmith@email.com', 12345678, 'user'),
+('bobross', 'bob', 'ross', 'bobby', 'bobross@email.com', 12345678, 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `area`
+--
+ALTER TABLE `area`
+  ADD PRIMARY KEY (`area_id`),
+  ADD UNIQUE KEY `area_id_UNIQUE` (`area_id`);
+
+--
+-- Indexes for table `connection`
+--
+ALTER TABLE `connection`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`username`),
+  ADD UNIQUE KEY `username_UNIQUE` (`username`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
