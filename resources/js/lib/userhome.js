306b3a96d66cd77bf50ec20928d8795d5caba03e
JustinSoh <hoggersoh@gmail.com>
2020-03-09 01:03:25 +0800

    Did pretty much nothing

--- resources/js/lib/userhome.js
+++ resources/js/lib/userhome.js
@@ -1,8 +1,4 @@
 
 $(document).ready(function() {
     $('.js-example-basic-single').select2();
-    $('#destinationInput').change(function() {
-        var currentDestinationOption = $( "select#destinationInput option:checked" ).val();
-        
-      });
 });
--- users/userhome.php
+++ users/userhome.php
@@ -1,25 +1,36 @@
 <!-- Stylesheet  -->
 <link rel="stylesheet" href="resources/css/userhome.css"></link>
 <section role="main" class="bg">
-    <section id="direction_container" class="row justify-content-center">
-        <article id="direction_header" class="justify-content-center" my-auto>
-            <h1 class="direction_text extra-bold">Enter your destination below <br></h1>
-        </article>
-    </section>
-    <section id="user_input_container" class="row">
-        <div class="col-sm-4 empty_div"></div>
-        <form id="user_form" class="col-sm-4">
+    <article id="user_input_container" class="my_container justify-content-center"> 
+        <h3 class="direction_text extra-bold">Great to see you <span class="highlight">#User</span>, Where would you like to go? <br></h3>
+        <form id="user_form">
             <div class="form-group ">
                 <select class="user_input js-example-basic-single" id="destinationInput">
-                    <option value="1">Singapore Expo</option>
-                    <option value="2">Suntec</option>
+                <?php
+                       $products = array("Singapore Expo", "Suntec");
+                       foreach($products as $item){
+                        ?><option value="<?php echo strtolower($item);?>"><?php echo $item; ?></option>
+                        <?php
+                        }
+                        ?>
                 </select>
             </div>
         </form>
-        <div class="col-sm-4 empty_div"></div>
-    </section>
-    <nav id="user_input_navigation" >
-        <span class="dot"></span>
-    </nav>
+        <form id="user_submit" >
+            <button type="button" class="next_button btn btn-primary">Next</button>
+        </form>
+    </article>
+
+    <!-- <section id="direction_container" class="row justify-content-center">
+        <article id="direction_header" class="w-100" my-auto>
+            
+        </article>
+    </section> -->
+    <!-- <section id="user_input_container" class="row">
+        <div class="col-sm-2 empty_div"></div>
+      
+        <div class="col-sm-2 empty_div"></div>
+    </section> -->
+
 </section>
 
