0801d928331666daa0897a7eb74376a5e95d5fda
shun-h2 <how.shunhan@gmail.com>
2020-03-09 18:40:50 +0800

    Update connections.php
    Added variable to store SQL query results after connection is closed

--- connections.php
+++ connections.php
@@ -1,20 +1,28 @@
 <?php
-function establish_connection(){
-    $dbservername = "localhost";
-    $dbusername = "root";
-    $dbpassword = "";
 
-    $conn = new mysqli($dbservername, $dbusername, $dbpassword);
+$dbservername = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "carpark";
+$users = array();
 
-    if ($conn->connect_error) {
-        die("Connection error: " . $conn->connect_error);
+$conn = new mysqli($dbservername, $dbusername, $dbpassword, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection error: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while ($row = $result->fetch_assoc()) {
+        $users[] = $row;
     }
-
-    return $conn;
+} else {
+    echo "0 results";
 }
 
-function kill_connection($conn){
-    $conn -> close();
-}
-   
+$conn->close();
 ?>
