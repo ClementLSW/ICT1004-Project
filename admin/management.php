9249940fecf4b35b8eb57749dddd9d8686f39d9e
shun-h2 <how.shunhan@gmail.com>
2020-03-09 10:18:30 +0800

    Create management.php
    Admin management page

---
+++ admin/management.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <?php
+        include "../header.inc.php";
+        ?>
+    </head>
+    <body>
+        <?php
+        include "../navigation.php";
+        $dbservername = "localhost";
+        $dbusername = "root";
+        $dbpassword = "";
+
+        $conn = new mysqli($dbservername, $dbusername, $dbpassword);
+
+        if ($conn->connect_error) {
+            die("Connection error: " . $conn->connect_error);
+        }
+
+        $sql = "SELECT * FROM carpark.users";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            ?>
+            <table id="myTable" class="display" style="width:100%;">
+                <thead>
+                    <tr>
+                        <th>Username</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Password</th>
+                        <th>Email</th>
+                        <th>Contact</th>
+                        <th>Permissions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<tr style='color:black;'>"
+                        . "<td>" . $row['username'] . "</td>"
+                        . "<td>" . $row['fname'] . "</td>"
+                        . "<td>" . $row['lname'] . "</td>"
+                        . "<td>" . $row['password'] . "</td>"
+                        . "<td>" . $row['email'] . "</td>"
+                        . "<td>" . $row['contact'] . "</td>"
+                        . "<td>" . $row['permissions'] . "</td>"
+                        . "</tr>";
+                    }
+                    ?>
+                </tbody>
+            </table>     
+            <script>
+                $(document).ready(function () {
+                    $('#myTable').DataTable();
+                });
+            </script>
+
+            <?php
+        }
+        ?>
+
+    </body>
+</html>
