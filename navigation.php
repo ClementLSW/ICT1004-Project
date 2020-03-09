ce6de65c11a0fd3b33cbbcf5c7de91c446bc362c
shun-h2 <how.shunhan@gmail.com>
2020-03-09 14:56:38 +0800

    Update navigation.php
    - Updated navigation.php with new link

--- navigation.php
+++ navigation.php
@@ -15,6 +15,9 @@
             <li class="nav-item">
                 <a class="nav-link" href="register.php">Register</a>
             </li>
+            <li class="nav-item">
+                <a class="nav-link" href="Manage.php">Manage</a>
+            </li>
         </ul>
     </div>
 </nav>
