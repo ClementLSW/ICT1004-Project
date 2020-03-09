e54faceca3a8d332848782948632863c64fa45cb
shun-h2 <how.shunhan@gmail.com>
2020-03-09 10:18:51 +0800

    Update header.inc.php
    Addeed additional links for datatables

--- header.inc.php
+++ header.inc.php
@@ -12,5 +12,12 @@
 <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/darkly/bootstrap.min.css" rel="stylesheet" integrity="sha384-rCA2D+D9QXuP2TomtQwd+uP50EHjpafN+wruul0sXZzX/Da7Txn4tB9aLMZV4DZm" crossorigin="anonymous">
 <link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet" />
 
+<!-- DataTables -->
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
+
+<script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+
+
 <!-- Own Style Sheet -->
 <link rel="stylesheet" href="resources/css/main.css"></link>
