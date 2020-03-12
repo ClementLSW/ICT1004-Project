e9cb345e3cceef1ff226812ba95818f08aa26c7a
shun-h2 <how.shunhan@gmail.com>
2020-03-12 23:59:58 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,15 @@
 <html lang="en">
     <head>
         <?php
-            include "header.inc.php";
-            include 'connections.php';?>
-    
+        include "header.inc.php";
+        include 'connections.php';
+        ?>        
+        <script src="resources/js/select2.min.js"></script>
+        <link href="resources/css/select2.min.css" rel="stylesheet" />
+
     </head>
     <body>
-        <?php include 'navigation.php';?>
-        <?php include 'users/userhome.php';?>
+        <?php include 'navigation.php'; ?>
+<?php include 'users/userhome.php'; ?>
     </body>
 </html>
