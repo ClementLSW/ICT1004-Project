4c1b1f7ab45e25e05430f480a37060fad747f5a3
shun-h2 <how.shunhan@gmail.com>
2020-03-13 23:33:40 +0800

    Update process_login.php
    Turned off error reporting

--- login/process_login.php
+++ login/process_login.php
@@ -1,56 +1,58 @@
 <html>
-<head>
-        <?php include '../header.inc.php'; ?>
+    <head>
+        <?php
+        //turn off error reporting
+        error_reporting(0);
+        include '../header.inc.php';
+        ?>
     </head>
     <body>
-        <?php include 'navigation.php';?>
+        <?php include 'navigation.php'; ?>
         <main class = "container">
             <?php
             $password = $_POST["pwd"];
             $username = $_POST["username"];
+
             /** Helper function to authenticate the login. */
             function authenticateUser() {
-            global $username, $firstname, $lastname, $email, $password, $errorMsg, $success;
+                global $username, $firstname, $lastname, $email, $password, $errorMsg, $success;
 
 // Create database connection.
-            $config = parse_ini_file('/var/www/private/db-config.ini');
-            $conn = new mysqli($config['servername'], $config['username'], $config['password'], $config['dbname']);
+                $config = parse_ini_file('/var/www/private/db-config.ini');
+                $conn = new mysqli($config['servername'], $config['username'], $config['password'], $config['dbname']);
 // Check connection
-            if ($conn->connect_error) {
-            $errorMsg = "Connection failed: " . $conn->connect_error;
-            $success = false;
-            } else {
-            $sql = "SELECT * FROM users WHERE ";
-            $sql .= "username='$username' AND password='$password'";
-            }
+                if ($conn->connect_error) {
+                    $errorMsg = "Connection failed: " . $conn->connect_error;
+                    $success = false;
+                } else {
+                    $sql = "SELECT * FROM users WHERE ";
+                    $sql .= "username='$username' AND password='$password'";
+                }
 // Execute the query
 
-            $result = $conn->query($sql);
-            
-            if ($result->num_rows > 0 || $username = $row["username"] || $password = $row["password"]) {
-            session_start(); 
-            $row = $result->fetch_assoc();
-            $firstname = $row["fname"];
-            $lastname = $row["lname"];
-            $_SESSION["username"] = $username;
-            echo "<h3>Login successful!</h3>";
-            echo "<h4>Welcome back, $firstname $lastname. </h4>";
-            echo "<form action = '../index.php'>";
-            echo "<button class='btn btn-success'>Return to Home</button></form> ";
-            
-            }
+                $result = $conn->query($sql);
 
-            else{
-            $success = false;
-            $errorMsg = "Email not found or password doesn't match...";
-            echo"<h3>Oops!</h3>";
-            echo "<h4>The following input errors were detected:</h4> <p>$errorMsg</p>";
-            echo "<form action = 'Login.php'>";
-            echo "<button class='btn btn-success'>Return to Login</button></form> ";
-            }
-            $result->free_result();
+                if ($result->num_rows > 0 || $username = $row["username"] || $password = $row["password"]) {
+                    session_start();
+                    $row = $result->fetch_assoc();
+                    $firstname = $row["fname"];
+                    $lastname = $row["lname"];
+                    $_SESSION["username"] = $username;
+                    echo "<h3>Login successful!</h3>";
+                    echo "<h4>Welcome back, $firstname $lastname. </h4>";
+                    echo "<form action = '../index.php'>";
+                    echo "<button class='btn btn-success'>Return to Home</button></form> ";
+                } else {
+                    $success = false;
+                    $errorMsg = "Email not found or password doesn't match...";
+                    echo"<h3>Oops!</h3>";
+                    echo "<h4>The following input errors were detected:</h4> <p>$errorMsg</p>";
+                    echo "<form action = 'Login.php'>";
+                    echo "<button class='btn btn-success'>Return to Login</button></form> ";
+                }
+                $result->free_result();
 
-            $conn->close();
+                $conn->close();
             }
 
             $functionName = "authenticateUser();";
