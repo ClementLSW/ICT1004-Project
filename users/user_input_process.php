352597e578ecc78fe6df15ce27c592c43119e6f8
shun-h2 <how.shunhan@gmail.com>
2020-03-13 23:34:52 +0800

    Update user_input_process.php
    Turned off error reporting

--- users/user_input_process.php
+++ users/user_input_process.php
@@ -1,30 +1,30 @@
-<?php 
-    include "../connections.php";
-    $currentDestination = null; 
-    $currentShop = null;
-    $products = [];
-    if(isset($_GET['destination'])){
-       if($_GET['destination'] == 1){
-           // Expo 
-           $connection = new connections();
-           $products = $connection -> retrieve_data_where("area" , "location_id" , $_GET['destination']);
-       }
-       if($_GET['destination'] == 2){
-           // Suntec City 
-           $connection = new connections();
-           $products = $connection -> retrieve_data_where("area" , "location_id" , $_GET['destination']);
-       }
-       
-       return $products;
+<?php
+
+//turn off error reporting
+error_reporting(0);
+include "../connections.php";
+$currentDestination = null;
+$currentShop = null;
+$products = [];
+if (isset($_GET['destination'])) {
+    if ($_GET['destination'] == 1) {
+        // Expo 
+        $connection = new connections();
+        $products = $connection->retrieve_data_where("area", "location_id", $_GET['destination']);
+    }
+    if ($_GET['destination'] == 2) {
+        // Suntec City 
+        $connection = new connections();
+        $products = $connection->retrieve_data_where("area", "location_id", $_GET['destination']);
     }
 
-    if (isset($_POST['currentDestination']) )
-    {
-        $currentDestination = $_POST['currentDestination'];
-    }
-    if (isset($_POST['currentShop']) )
-    {
-        $currentShop = $_POST['currentShop'];
-    }
+    return $products;
+}
 
+if (isset($_POST['currentDestination'])) {
+    $currentDestination = $_POST['currentDestination'];
+}
+if (isset($_POST['currentShop'])) {
+    $currentShop = $_POST['currentShop'];
+}
 ?>
