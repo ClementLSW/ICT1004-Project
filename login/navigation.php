12f4539c07ccf88cdb68ee4ed15fd4e9b0995f82
shun-h2 <how.shunhan@gmail.com>
2020-03-13 10:30:06 +0800

    Create navigation.php

---
+++ login/navigation.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<nav role="navigation" class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="index.php">LOGO</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="../index.php">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="login.php">Login<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="register.php">Register</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="../management.php">Manage</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<?php
+        session_start();
+        if(isset($_SESSION['username'])){
+        echo $_SESSION['username'];
+        echo "<br/><a href='logout.php'>logout</a>";
+        echo "<button class='btn btn-success'>Logout</button></form> ";
+}
+        ?>
