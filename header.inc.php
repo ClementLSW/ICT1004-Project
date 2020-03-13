62351e83eedef1fbd71f80da7078fc1a17013fff
Justin <hoggersoh@gmail.com>
2020-03-14 02:40:51 +0800

    changed the path for certain files

--- header.inc.php
+++ header.inc.php
@@ -6,8 +6,8 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
-<script src="/resources/js/lib/userhome.js"></script>
-<script src="/resources/js/lib/navigation.js"></script>
+<script src="resources/js/lib/userhome.js"></script>
+<script src="resources/js/lib/navigation.js"></script>
 
 <!-- External Style Sheet -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"></link>
@@ -24,5 +24,5 @@
 
 
 <!-- Own Style Sheet -->
-<link rel="stylesheet" href="/resources/css/main.css">
+<link rel="stylesheet" href="resources/css/main.css">
 </link>
