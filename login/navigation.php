deab392888e69e1098e83398d5e6c1a798758711
shun-h2 <how.shunhan@gmail.com>
2020-03-14 03:35:46 +0800

    Update navigation.php

--- login/navigation.php
+++ login/navigation.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <nav role="navigation" class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="index.php">LOGO</a>
+    <a class="navbar-brand" href="../index.php">LOGO</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
