e7b8d6f9f4da88e8a0f81536cc195fb7b314f020
Justin <hoggersoh@gmail.com>
2020-03-15 02:08:14 +0800

    Update header.inc.php

--- header.inc.php
+++ header.inc.php
@@ -6,8 +6,19 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
-<script src="/resources/js/lib/userhome.js"></script>
-<script src="/resources/js/lib/navigation.js"></script>
+
+<script> var loc = window.location.pathname;
+var script = document.createElement('script');
+var dir = loc.split('/'); 
+script.setAttribute('src','/' + dir[1] + '/' + "resources/js/lib/userhome.js" );
+document.getElementsByTagName('script')[0].parentNode.appendChild(script);
+</script>
+<script> var loc = window.location.pathname;
+var script = document.createElement('script');
+var dir = loc.split('/'); 
+script.setAttribute('src','/' + dir[1] + '/' + "resources/js/lib/navigation.js" );
+document.getElementsByTagName('script')[0].parentNode.appendChild(script);
+</script>
 
 <!-- External Style Sheet -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"></link>
@@ -24,5 +35,11 @@
 <link href="/resources/css/select2.min.css" rel="stylesheet" /> -->
 
 <!-- Own Style Sheet -->
-<link rel="stylesheet" href="/resources/css/main.css">
+<script> var loc = window.location.pathname;
+var link = document.createElement('link');
+var dir = loc.split('/'); 
+link.setAttribute('rel' , 'stylesheet');
+link.setAttribute('href','/' + dir[1] + '/' + "resources/css/main.css" );
+document.getElementsByTagName('link')[0].parentNode.appendChild(link);
+</script>
 </link>
