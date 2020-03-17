6525c1d174df0d155231f122e1efb1431563829f
Justin <hoggersoh@gmail.com>
2020-03-18 01:29:37 +0800

    Adding google map

---
+++ views/mapview.php
@@ -0,0 +1,9 @@
+
+<!-- <script src="//d3js.org/d3.geo.projection.v2.min.js"></script> -->
+<body id="mainbody" class="bg">
+        <div class="overlay">
+            <section class="w-100 map_container">
+                <div id="map-holder"></div>
+            </section>
+        </div>
+</body>
