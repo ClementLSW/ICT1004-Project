2657a9be3925a7aa7622ead773c08fcbd00e85c3
shun-h2 <how.shunhan@gmail.com>
2020-03-23 15:16:51 +0800

    Updated fonts

--- admin/header.inc.php
+++ admin/header.inc.php
@@ -9,14 +9,6 @@
 <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.bootstrap.min.css" type="text/css">                    
 <!-- <link rel="stylesheet" href="main.css" type="text/css"> -->
 <script> var loc = window.location.pathname;
-var link = document.createElement('link');
-var dir = loc.split('/'); 
-link.setAttribute('rel' , 'stylesheet');
-link.setAttribute('href','/' + dir[1] + '/' + "admin/main.css" );
-console.log(dir[1]);
-document.getElementsByTagName('link')[0].parentNode.appendChild(link);
-</script>
-<script> var loc = window.location.pathname;
 var script = document.createElement('script');
 var dir = loc.split('/'); 
 script.setAttribute('src','/' + dir[1] + '/' + "resources/js/lib/navigation.js" );
@@ -27,6 +19,14 @@ var link = document.createElement('link');
 var dir = loc.split('/'); 
 link.setAttribute('rel' , 'stylesheet');
 link.setAttribute('href','/' + dir[1] + '/' + "resources/css/main.css" );
+console.log(dir[1]);
+document.getElementsByTagName('link')[0].parentNode.appendChild(link);
+</script>
+<script> var loc = window.location.pathname;
+var link = document.createElement('link');
+var dir = loc.split('/'); 
+link.setAttribute('rel' , 'stylesheet');
+link.setAttribute('href','/' + dir[1] + '/' + "admin/main.css" );
 console.log(dir[1]);
 document.getElementsByTagName('link')[0].parentNode.appendChild(link);
 </script>
